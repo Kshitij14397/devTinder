1bc1a6c23e3301f685e2a5c64cc3227c77f08704
Kshitij Pandey <kshitij14397@gmail.com>
2025-03-13 22:44:10 +0530

    Added code

--- src/app.js
+++ src/app.js
@@ -2,16 +2,32 @@ const express = require('express');
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-    res.send("Hello Hello Hello!");
+const data = require('../data.json');
+console.log(data);
+
+// This will only handle GET call to /user
+app.get("/user/:userId", (req, res) => {
+    console.log("kp1", req.query);
+    console.log("kp2", req.params)
+    res.send({ firstName: "Kshitij", lastName: "Pandey" });
 });
 
-app.use("/test", (req, res) => {
-    res.send("Hello from the server!");
+app.post("/user", (req, res) => {
+    // saving data to DB
+    res.send("Data successfully saved to the database!");
 });
 
-app.use("/", (req, res) => {
-    res.send("Namaste from the dashboard!");
+app.delete("/user", (req, res) => {
+    res.send("Deleted successfully!");
+});
+
+// This will match all the HTTP method API calls to /ping
+app.use("/ping", (req, res) => {
+    res.send("pong")
+});
+
+app.get(/.*fly$/, (req, res) => {
+    res.send({ firstName: "Kshitij", lastName: "Pandey" });
 });
 
 app.listen(3000, () => {
