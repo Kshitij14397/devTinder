a81ced49ac730c2adcc2ce94f155f018174cdee8
Kshitij Pandey <kshitij14397@gmail.com>
2025-06-15 02:24:01 +0530

    Writing auth middleware

--- src/app.js
+++ src/app.js
@@ -1,39 +1,25 @@
 const express = require("express");
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
 const app = express();
 
-// app.use("/", (req, res) => {
-//   res.send("Hello");
-// });
+app.use("/admin", adminAuth);
 
-app.use(
-  "/user",
-  [
-    (req, res, next) => {
-      console.log("Handling the router user!!");
-      next();
-    },
-    (req, res, next) => {
-      console.log("Handling the router user 2!!");
-      // res.send("2nd Response!!");
-      next();
-    },
-  ],
-  (req, res, next) => {
-    console.log("Handling the router user 3!!");
-    // res.send("3rd Response!!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the router user 4!!");
-    // res.send("4th Response!!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the router user 5!!");
-    res.send("5th Response!!");
-  }
-);
+app.get("/user/login", (req, res) => {
+  res.send("User LoggedIn Successfully");
+});
+
+app.get("/user/data", userAuth, (req, res) => {
+  res.send("User Data Sent");
+});
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All Data Sent");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("Deleted a user");
+});
 
 app.listen(3000, () => {
   console.log("Server is listening on PORT 3000");
