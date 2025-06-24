82543a97778b8ba52ffee8a4b2b37d94da7ef928
Kshitij Pandey <kshitij14397@gmail.com>
2025-06-24 05:58:56 +0530

    Completed Episode-09 | Encrypting Passwords

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,10 +18,20 @@ const userSchema = new mongoose.Schema(
       unique: true,
       lowercase: true,
       trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email");
+        }
+      },
     },
     password: {
       type: String,
       required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a strong password");
+        }
+      },
     },
     age: {
       type: Number,
@@ -38,6 +49,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       default:
         "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Incorrect URL");
+        }
+      },
     },
     about: {
       type: String,
