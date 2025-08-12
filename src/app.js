bd4ed8fdb121de68ff345b709d95593f2bb3cb79
Kshitij Pandey <kshitij14397@gmail.com>
2025-08-13 01:44:40 +0530

    Completed Episode-06 | Scheduling Cron Jobs

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+require("./utils/cronjob");
 
 const app = express();
 
---
+++ src/utils/cronjob.js
@@ -0,0 +1,45 @@
+const cron = require("node-cron");
+const { subDays, startOfDay, endOfDay } = require("date-fns");
+const sendEmail = require("./sendEmail");
+const ConnectionRequest = require("../models/connectionRequest");
+
+// This job will run at 8 AM everyday.
+cron.schedule("0 8 * * *", async () => {
+  // Send emails to all people who got requests the previous day
+  try {
+    const yesterday = subDays(new Date(), 1);
+    const yesterdayStart = startOfDay(yesterday);
+    const yesterdayEnd = endOfDay(yesterday);
+
+    console.log(yesterday, yesterdayStart, yesterdayEnd);
+
+    const pendingRequests = await ConnectionRequest.find({
+      status: "interested",
+      createdAt: {
+        $gte: yesterdayStart,
+        $lt: yesterdayEnd,
+      },
+    }).populate("fromUserId toUserId");
+
+    const listOfEmails = [
+      ...new Set(pendingRequests.map((req) => req?.toUserId?.emailId)),
+    ];
+
+    console.log(listOfEmails);
+
+    for (const email of listOfEmails) {
+      // send Emails
+      try {
+        const res = await sendEmail.run(
+          "New Friend Requests pending for " + email,
+          "There are so many friend requests pending, please login to devtinderpro.pw/login to accept or reject requests."
+        );
+        console.log(res);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
